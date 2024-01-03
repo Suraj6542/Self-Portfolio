3d9ac12017058ef968f381da52fdc51572556897
Your Name <your@email.com>
2024-01-03 18:58:49 +0530

    all the contact data are grtting upload into an excel file

--- portfolio/portfolioapp/views.py
+++ portfolio/portfolioapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import ContactMessage
 from django.contrib import messages
+import pandas as pd
 
 def index(request):
     if request.method=='POST':
@@ -9,6 +10,14 @@ def index(request):
         Description=request.POST['Description']
         ContactMessage.objects.create(name=Name, email=Email, message=Description)
         messages.success(request,'Data has been submitted')
+        export_data_to_excel()
 
     return render(request,'index.html')
+
+def export_data_to_excel():
+    queryset = ContactMessage.objects.all()
+    df = pd.DataFrame(list(queryset.values()))
+    excel_file_path = 'C:/Users/hp/Downloads/Portfolio_data.xlsx'
+    df.to_excel(excel_file_path, index=False)
+
     
