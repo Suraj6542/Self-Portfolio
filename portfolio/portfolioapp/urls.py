7216b25c7502b39af9d3577102e6eee07d601eb7
Your Name <your@email.com>
2024-07-28 10:20:46 +0530

    Your commit message

--- portfolio/portfolioapp/urls.py
+++ portfolio/portfolioapp/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from portfolioapp import views
 from .views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name="index"),
     # path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/portfolioapp/views.py
+++ portfolio/portfolioapp/views.py
@@ -4,15 +4,21 @@ from django.contrib import messages
 import pandas as pd
 
 def index(request):
-    if request.method=='POST':
-        Name=request.POST['Name']
-        Email=request.POST['Email']
-        Description=request.POST['Description']
-        ContactMessage.objects.create(name=Name, email=Email, message=Description)
-        messages.success(request,'Data has been submitted')
-        export_data_to_excel()
+    if request.method == 'POST':
+        Name = request.POST.get('Name')
+        Email = request.POST.get('Email')
+        Description = request.POST.get('Description')
 
-    return render(request,'index.html')
+        if Name and Email and Description:
+            ContactMessage.objects.create(name=Name, email=Email, message=Description)
+            messages.success(request, 'Data has been submitted')
+            # export_data_to_excel()
+        else:
+            messages.error(request, 'All fields are required')
+        
+        return redirect('index')
+
+    return render(request, 'index.html')
 
 def export_data_to_excel():
     queryset = ContactMessage.objects.all()
